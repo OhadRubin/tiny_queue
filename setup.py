9b87d56cdeb1b761d25d0ea80cdb808385c6975b
Your Name <you@example.com>
2022-10-26 13:25:20 +0000

    added try except

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description="",
     long_description_content_type="text/plain",
     url="",
-    packages=["tiny_queue","tiny_queue.connections"],
+    packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
     python_requires=">=3.6",
--- tiny_queue/agent.py
+++ tiny_queue/agent.py
@@ -4,18 +4,22 @@ from loguru  import logger
 import subprocess
 from tiny_queue.connections.redis_connection import RedisConnection
 from tiny_queue.connections.sqlite_connection import SqliteConnection
-
+import time
 
 
 def get_next_task(conn):
     while True:
+        time.sleep(1)
         with conn.get_queue() as queue_datebase:
-            if b"queue" in queue_datebase:
-                queue = queue_datebase[b'queue']
-                if len(queue)>0:
-                    next_task = queue.pop()
-                    queue_datebase[b'queue'] = queue
-                    return next_task
+            try:
+                if b"queue" in queue_datebase:
+                    queue = queue_datebase[b'queue']
+                    if len(queue)>0:
+                        next_task = queue.pop()
+                        queue_datebase[b'queue'] = queue
+                        return next_task
+            except:
+                pass
                 
 def agent_loop(queue="redis"):
     logger.info(f"Starting agent")
